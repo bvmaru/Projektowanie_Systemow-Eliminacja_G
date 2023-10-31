c4b3fd003bf3fbf96a4b62f7e6f0ab085e54e813
Marcel <bvmaru@proton.me>
2023-10-31 21:37:28 +0100

    time measurements

---
+++ GaussElimination.cs
@@ -0,0 +1,40 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eliminacja_G
+{
+    public class GaussElimination
+    {
+        public Matrix<double> aMatrix { get; set; }
+        public Matrix<double> mMatrix { get; set; }
+        public int nSize { get; set; }
+
+
+        public void Calculate()
+        {
+            for (int i = 0; i < nSize - 1; i++)
+            {
+                for (int j = i + 1; j < nSize; j++)
+                {
+                    if (aMatrix[i, i] != 0)
+                    {
+                        mMatrix[j, i] = -aMatrix[j, i] / aMatrix[i, i];
+                    }
+                    else
+                    {
+                        mMatrix[j, i] = 0;
+                    }
+
+                    for (int k = i + 1; k < nSize + 1; k++)
+                    {
+                        aMatrix[j, k] = aMatrix[j, k] + mMatrix[j, i] * aMatrix[i, k];
+                    }
+                }
+            }
+        }
+    }
+}
